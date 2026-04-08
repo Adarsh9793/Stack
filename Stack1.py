50b20d0ed08e0ae108f6448195709d92411afd80
Adarsh tripathi <adarshtripathi9793085@gmail.com>
2026-04-08 19:32:04 +0530

    Add files via upload

---
+++ Stack1.py
@@ -0,0 +1,7 @@
+myStack = []
+myStack.append('A')
+myStack.append('B') 
+myStack.append('C')
+print(myStack)  # Output: ['A', 'B', 'C']
+print(myStack.pop())  # Output: 'C'
+print(myStack)  # Output: ['A', 'B']
